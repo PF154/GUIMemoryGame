81f3233647b6169d4a4e16ef8bfd18255fb94d85
peter <pwforsberg@wm.edu>
2023-12-11 13:16:04 -0500

    Added alphabet interface and class implementation

---
+++ src/edu/wm/cs/cs301/guimemorygame/model/LatinAlphabet.java
@@ -0,0 +1,8 @@
+package edu.wm.cs.cs301.guimemorygame.model;
+
+class LatinAlphabet implements Alphabet {
+	public char[] toCharArray() {
+		return new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+				'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '2','3', '4'};
+	};
+}
--- src/edu/wm/cs/cs301/guimemorygame/model/MemoryModel.java
+++ src/edu/wm/cs/cs301/guimemorygame/model/MemoryModel.java
@@ -4,8 +4,18 @@ public class MemoryModel {
 	
 	private MemoryCard[][] memoryGrid;
 	
+	private Alphabet alphabet;
+	
+	private int rows, columns;
+	
 	public MemoryModel() {
-		
+		this.alphabet = new LatinAlphabet();
+		this.rows = 3;
+		this.columns = 4;
+	}
+	
+	private void intitializeMemoryGrid() {
+		MemoryCard[][] memoryGrid = new MemoryCard[rows][columns];
 	}
 	
 }
