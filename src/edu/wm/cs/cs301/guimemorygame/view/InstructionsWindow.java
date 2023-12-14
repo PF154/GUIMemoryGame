e7ee074f190d1caacc0ef61b975c337f9b8bc85a
peter <pwforsberg@wm.edu>
2023-12-14 14:59:01 -0500

    Added label with htm text to instructions window

--- src/edu/wm/cs/cs301/guimemorygame/view/InstructionsWindow.java
+++ src/edu/wm/cs/cs301/guimemorygame/view/InstructionsWindow.java
@@ -1,14 +1,50 @@
 package edu.wm.cs.cs301.guimemorygame.view;
 
+import java.awt.BorderLayout;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class InstructionsWindow extends JDialog {
 	
 	public InstructionsWindow(MemoryFrame view) {
 		super(view.getFrame(), "Instructions", true);
 		
+		add(instructionsPanel(), BorderLayout.CENTER);
+		
 		pack();
 		setLocationRelativeTo(view.getFrame());
 		setVisible(true);
+	}
+	
+	private JPanel instructionsPanel() {
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+		
+		JLabel instructionsText = new JLabel();
+		
+		String sourceText;
+		try {
+			sourceText = Files.readString(Paths.get("resources\\instructions.htm"), StandardCharsets.UTF_8);
+			
+			System.out.println(sourceText);
+			
+			instructionsText.setText(sourceText);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			instructionsText.setText("Couldn't get instructions");
+		}
+		
+		panel.add(instructionsText);
+		
+		return panel;
 	}
 }
